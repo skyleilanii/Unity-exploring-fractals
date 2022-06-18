33b8bdd8a7012b6ee6c781aff123d77302804cd9
sky <107776367+skyleilanii@users.noreply.github.com>
2022-06-18 19:35:33 -0400

    Create SerialCommunicationSketch.ino

---
+++ Arduino/SerialCommunicationSketch.ino
@@ -0,0 +1,25 @@
+void setup(){ 
+  Serial.begin(9600);  // set baud rate 
+
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+
+  // set default to be internal pullup resistor enabled 
+  digitalWrite(6, HIGH);
+  digitalWrite(7, HIGH);
+}
+
+void loop() { 
+  
+  if(digitalRead(6) == LOW) {    
+    Serial.write(2); 
+    Serial.flush(); 
+    delay(10);
+  } 
+  
+  else if (digitalRead(7) == LOW){ 
+    Serial.write(1);
+    Serial.flush();
+    delay(10);
+  }  
+}
