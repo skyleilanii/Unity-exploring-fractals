0a66a844826417027c7dc8ed8b5b725cd74d0dfd
sky <107776367+skyleilanii@users.noreply.github.com>
2022-06-18 19:42:25 -0400

    Create PlayerCollision.cs

---
+++ Scripts/other/PlayerCollision.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    public Rigidbody rb; 
+    protected int Timer;
+    protected float sidewaysForce = 500f;
+    protected float m_Thrust = 20f;
+
+    private void OnCollisionEnter(Collision collisionInfo)
+    {
+       if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = false;
+        }
+    }
+
+    private void OnCollisionExit(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            movement.enabled = true;
+            Timer = 0;
+        }
+    }
+
+    private void OnCollisionStay(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            //Debug.Log("in contact w collider ");
+            Timer += 1;
+           // Debug.Log(Timer);
+
+            // if the player's rigidbody has been in contact with the Obstacle object for
+            // more than 3 seconds, I want there to be a force to stop it from staying in contact 
+            // 
+            if( Timer > 3)
+            {
+                rb.AddForce(transform.up * m_Thrust);
+            }
+            
+            if (Input.GetKey("d"))
+            {
+                Debug.Log("collisionStay d");
+                rb.AddForce(-sidewaysForce* 2 * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+
+                movement.enabled = true;               
+            }
+
+            if (Input.GetKey("a"))
+            {
+                Debug.Log("collisionStay a");
+                movement.enabled = true;
+            }
+        }
+    }
+}
